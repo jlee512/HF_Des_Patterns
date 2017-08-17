b93225e626dc31697c9f89b56449b89cbb6c92cd
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-17 14:05:56 +1200

    HeatIndexDisplay - class added

---
+++ src/Observer_Pattern/WeatherStation.java
@@ -0,0 +1,22 @@
+package Observer_Pattern;
+
+/**
+ * Created by Julian on 17/08/2017.
+ */
+public class WeatherStation {
+    //This implemenation is symbolic of weather station hardware sending new data for the purposes of testing our observer pattern
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+
+}
