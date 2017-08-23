425f85150fd6eaca83f60b7f6cf12c9deb201590
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-23 14:06:39 +1200

    Decorator Pattern - completed - widespread use in Java IO Package

---
+++ src/Decorator_Pattern/InputTest.java
@@ -0,0 +1,29 @@
+package Decorator_Pattern;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Julian on 23/08/2017.
+ */
+public class InputTest {
+
+    public static void main(String[] args) throws IOException {
+        int c;
+
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("decorator_test.txt")));
+
+            while ((c = in.read()) >= 0 ) {
+                System.out.print((char)c);
+            }
+
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
